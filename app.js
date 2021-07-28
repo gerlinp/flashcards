2262634da70038f4f26e65edfca87f554f8ff616
gerlinp <gerlin.pierrelouis@gmail.com>
2021-07-28 11:16:25 -0400

    complete

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false}))
-app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+app.use('/static', express.static('public'));
 
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 
 const mainRoutes = require('./routes');
 const cardRoutes = require('./routes/cards');
@@ -16,17 +17,17 @@ app.use(mainRoutes);
 app.use('/cards', cardRoutes);
 
 app.use((req, res, next) => {
-    const err = new Error('Not Found')
-    err.status = 404;
-    next(err);
-})
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-app.use((err, req , res, next) => {
-    res.locals.error = err;
-    res.status(err.status);
-    res.render('error');
-})
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
+});
 
 app.listen(3000, () => {
-    console.log('The application is running on local:3000')
-})
+    console.log('The application is running on localhost:3000!')
+});
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,32 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
     const name = req.cookies.username;
     if (name) {
-        res.render('index', {name})
+      res.render('index', { name });
     } else {
-        res.redirect('/hello')
+      res.redirect('/hello');
     }
-    
 });
 
-
-
 router.get('/hello', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-        res.redirect('/')  
-    } else {
-        res.render('hello')
-    }
-})
+  const name = req.cookies.username;
+  if (name) {
+    res.redirect('/');
+  } else {
+    res.render('hello');
+  }
+});
 
 router.post('/hello', (req, res) => {
-    res.cookie('username', req.body.username);
-    res.redirect('/');
-})
+  res.cookie('username', req.body.username);
+  res.redirect('/');
+});
 
-router.post('/goodbye', (req,res) => {
-    res.clearCookie('username')
-    res.redirect('/hello')
-})
+router.post('/goodbye', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/hello');
+});
 
-module.exports = router
+module.exports = router;
